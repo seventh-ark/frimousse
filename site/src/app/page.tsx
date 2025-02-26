cc693ffddb612482a6f916300182c533640aa3c3
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-02-26 14:24:28 +0100

    Customize playground picker

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { EmojiPicker, type Locale } from "frimousse";
+import { FrownIcon, LoaderCircleIcon } from "lucide-react";
 import { useState } from "react";
 import pkg from "../../../package.json";
 
@@ -39,65 +40,72 @@ export default function Page() {
         </select>
         {isOpen && (
           <EmojiPicker.Root
-            className="w-fit"
+            className="w-fit rounded-lg border border-gray-200 bg-white"
             columns={columns}
             locale={locale}
             onEmojiSelect={(emoji) => console.log(emoji)}
-            skinTone="medium-dark"
           >
             <EmojiPicker.Search />
-            <EmojiPicker.Viewport className="h-[400px]">
+            <EmojiPicker.Viewport className="h-[320px]">
               <EmojiPicker.Loading>
-                <span>Loading…</span>
+                <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+                  <LoaderCircleIcon className="size-6 animate-spin" />
+                  <span>Loading…</span>
+                </div>
               </EmojiPicker.Loading>
               <EmojiPicker.Empty>
-                {({ search }) => <div>No emoji found for "{search}".</div>}
+                <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+                  <FrownIcon className="size-6" />
+                  <span>No emoji found.</span>
+                </div>
               </EmojiPicker.Empty>
               <EmojiPicker.List
+                className="select-none"
                 components={{
                   Row: ({ children, ...props }) => (
-                    <div {...props} className="px-5">
+                    <div {...props} className="scroll-mb-1 px-1">
                       {children}
                     </div>
                   ),
                   Emoji: ({ emoji, isActive, ...props }) => (
                     <button
                       {...props}
-                      style={{
-                        backgroundColor: isActive ? "red" : "transparent",
-                      }}
+                      aria-label={emoji.label}
+                      className="flex size-7 cursor-pointer items-center justify-center rounded-md transition duration-100 data-[active]:bg-gray-100 data-[active]:duration-0"
                     >
-                      {emoji.emoji}
+                      <span className="whitespace-nowrap">{emoji.emoji}</span>
                     </button>
+                  ),
+                  CategoryHeader: ({ category, ...props }) => (
+                    <div
+                      {...props}
+                      className="bg-white px-2 pt-2 pb-1.25 font-semibold text-gray-400 text-xs uppercase"
+                    >
+                      {category.label}
+                    </div>
                   ),
                 }}
               />
             </EmojiPicker.Viewport>
-            <EmojiPicker.ActiveEmoji>
-              {({ emoji }) =>
-                emoji ? (
-                  <div>
-                    {emoji.emoji} - {emoji.label} - {emoji.shortcode}
-                  </div>
-                ) : null
-              }
-            </EmojiPicker.ActiveEmoji>
-            <EmojiPicker.SkinTone>
-              {({ skinTone, skinTones, setSkinTone }) => (
-                <div>
-                  {skinTones.map(({ skinTone, emoji }) => (
-                    <button
-                      key={skinTone}
-                      onPointerEnter={() => setSkinTone(skinTone)}
-                      type="button"
-                    >
-                      {emoji}
-                    </button>
-                  ))}
-                  {skinTone}
-                </div>
-              )}
-            </EmojiPicker.SkinTone>
+            <div className="flex w-full min-w-0 flex-1 border-gray-200 border-t p-2">
+              <EmojiPicker.ActiveEmoji>
+                {({ emoji }) => (
+                  <>
+                    <div className="-ml-1 mr-1 flex aspect-square w-9 items-center justify-center text-2xl">
+                      {emoji?.emoji}
+                    </div>
+                    <div className="flex w-0 min-w-0 flex-1 flex-col justify-center text-xs">
+                      <span className="truncate font-semibold text-gray-500">
+                        {emoji?.label}
+                      </span>
+                      <span className="truncate text-gray-400">
+                        {emoji?.shortcode}
+                      </span>
+                    </div>
+                  </>
+                )}
+              </EmojiPicker.ActiveEmoji>
+            </div>
           </EmojiPicker.Root>
         )}
       </main>
