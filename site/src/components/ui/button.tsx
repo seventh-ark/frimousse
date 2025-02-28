7cf71fd916a8db5be6407ab7e2b4f3dc3195a272
Le Vaillant Pierre <pierre.levaillant@liveblocks.io>
2025-02-28 09:46:42 +0100

    Clean button

--- site/src/components/ui/button.tsx
+++ site/src/components/ui/button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps
 }
 
 const buttonVariants = cva(
-  "transition-colors duration-200 ease-out inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "transition-colors duration-200 ease-out inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring focus-visible:ring-[3px]",
   {
     variants: {
       variant: {
