3c27f4c68d4a053108d9f2da17c337833cb17c68
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-02-28 15:41:43 +0100

    Fix overflow issues

--- site/src/components/reactions.tsx
+++ site/src/components/reactions.tsx
@@ -45,12 +45,13 @@ export function Reactions({
   return (
     <div
       className={cn(
-        "flex flex-wrap gap-(--gap) overflow-hidden",
-        "[--gap:var(--spacing)] lg:[--gap:calc(var(--spacing)*1.5)]",
+        "flex flex-wrap gap-(--gap)",
+        "[--gap:var(--spacing)] lg:[--gap:calc(var(--spacing)_*_1.5)]",
         className,
       )}
       style={{
-        height: `calc(var(--spacing) * 8 * ${MAX_ROWS} + var(--gap) *${MAX_ROWS - 1})`,
+        height: `calc(var(--spacing) * 8 * ${MAX_ROWS} + var(--gap) * ${MAX_ROWS - 1})`,
+        clipPath: "inset(-3px -3px -3px -3px)",
       }}
       {...props}
     >
